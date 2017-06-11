8db399c22fc0b31d7b4a8b6b44431be93ff0f93d
shrutiburnwal <shruti.nitdgp@gmail.com>
2017-06-11 15:29:15 +0530

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -1,32 +1,149 @@
-var timoutWarning = 240000; // Display warning in 4 Mins.
-var timoutNow = 300000; // Timeout in 5 mins.
-var logoutUrl = 'http://facebook.com/logout.php'; // URL to logout page.
+var timer;
+window.onblur = function () { 
+ 
+clearInterval(timer);
+   timer = window.setTimeout(FacebookLogout, 6000);
 
-var warningTimer;
-var timeoutTimer;
+}; 
 
-// Start timers.
-function StartTimers() {
-    warningTimer = setTimeout("IdleWarning()", timoutWarning);
-    timeoutTimer = setTimeout("IdleTimeout()", timoutNow);
-}
+document.onclick= function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
 
-// Reset timers.
-function ResetTimers() {
-    clearTimeout(warningTimer);
-    clearTimeout(timeoutTimer);
-    StartTimers();
-    $("#timeout").dialog('close');
-}
+document.onmousemove = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
 
-// Show idle timeout warning dialog.
-function IdleWarning() {
-    $("#timeout").dialog({
-        modal: true
+
+document.onkeydown = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+
+window.onfocus = function () { 
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+function FacebookLogout() {
+
+	//var insertedNodes = [];
+	
+	var logout;
+    
+    var observer = new WebKitMutationObserver(function(mutations) {
+             mutations.forEach(function(mutation) {
+             for (var i = 0; i < mutation.addedNodes.length; i++)
+                      insertedNodes.push(mutation.addedNodes[i]);
+                       console.log(mutation.addedNodes[i]);
+             })
+             
+             logout = document.getElementsByClassNvar timer;
+window.onblur = function () { 
+ 
+clearInterval(timer);
+   timer = window.setTimeout(FacebookLogout, 6000);
+
+}; 
+
+document.onclick= function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+document.onmousemove = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+
+document.onkeydown = function() {
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+
+window.onfocus = function () { 
+clearInterval(timer);
+timer = window.setTimeout(FacebookLogout, 6000);
+};
+
+function FacebookLogout() {
+
+	//var insertedNodes = [];
+	
+	var logout;
+    
+    var observer = new WebKitMutationObserver(function(mutations) {
+             mutations.forEach(function(mutation) {
+             for (var i = 0; i < mutation.addedNodes.length; i++)
+                      insertedNodes.push(mutation.addedNodes[i]);
+                       console.log(mutation.addedNodes[i]);
+             })
+             
+             logout = document.getElementsByClassName("_w0d")[0];
+
+             if(logout!=null){
+    	             observer.disconnect();
+			         console.log("llll");
+			         logout.submit();
+	         }
+    
+            
+    
     });
-}
+     observer.observe(document, { 
+    	            childList: true,
+    	            subtree : true,
+    	            characterData: true,
+    	            attributes: true
+    });
 
-// Logout the user.
-function IdleTimeout() {
-    window.location = logoutUrl;
+	var logoutbutton = document.getElementById("userNavigationLabel");
+	
+	if(logoutbutton == null){
+		 
+		 console.log("it is nessa's code");
+	
+	} else {
+		
+		//alert("we need to logout now");
+		logoutbutton.click();
+	}
+    
+}
+     
+    ame("_w0d")[0];
+
+             if(logout!=null){
+    	             observer.disconnect();
+			         console.log("llll");
+			         logout.submit();
+	         }
+    
+            
+    
+    });
+     observer.observe(document, { 
+    	            childList: true,
+    	            subtree : true,
+    	            characterData: true,
+    	            attributes: true
+    });
+
+	var logoutbutton = document.getElementById("userNavigationLabel");
+	
+	if(logoutbutton == null){
+		 
+		 console.log("it is nessa's code");
+	
+	} else {
+		
+		alert("we need to logout now");
+		logoutbutton.click();
+	}
+    
 }
