b7160140fa253f4d6b9393819ae2a1747dfaa44f
shrutiburnwal <shruti.nitdgp@gmail.com>
2017-06-11 15:39:46 +0530

    Add files via upload

---
+++ Twitter.js
@@ -0,0 +1,50 @@
+var timer;
+window.onblur = function () { 
+ 
+clearInterval(timer);
+   timer = window.setInterval(TwitterLogout, 6000);
+
+}; 
+
+document.onclick= function() {
+clearInterval(timer);
+timer = window.setInterval(TwitterLogout, 6000);
+};
+
+document.onmousemove = function() {
+clearInterval(timer);
+timer = window.setInterval(TwitterLogout, 6000);
+};
+
+
+document.onkeydown = function() {
+clearInterval(timer);
+timer = window.setInterval(TwitterLogout, 6000);
+};
+document.onscroll = function() {
+clearInterval(timer);
+timer = window.setInterval(TwitterLogout, 6000);
+};
+
+window.onfocus = function () { 
+clearInterval(timer);
+timer = window.setInterval(TwitterLogout, 6000);
+};
+
+
+
+function TwitterLogout() {
+	var toggledownbutton = document.getElementById("user-dropdown-toggle");
+	if(toggledownbutton==null){
+		console.log("ok");
+	}
+	else{
+		toggledownbutton.click();
+		var signoutform = document.getElementById("signout-form");
+		if(signoutform==null){
+			alert("cannot logout");
+		}else{
+			signoutform.submit();
+		}
+	}
+}
